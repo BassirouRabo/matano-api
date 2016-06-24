755c1f14797fff70420123a6b5029b014327b62e
smalllamartin <smalllamartin@gmailcom>
2016-06-24 20:11:41 +0100

    webSocketClient

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -2,19 +2,32 @@ package controllers;
 
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
+import play.mvc.LegacyWebSocket;
 import play.mvc.Result;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import play.mvc.WebSocket;
 
 
 public class Home extends Controller {
 
     @Transactional
     public Result index() {
-        Date date = new Date();
-        String jour = new SimpleDateFormat("dd/MM/yyyy").format(date);
-        return ok("matano " + jour);
+        return ok("matano ?");
+    }
+
+    public LegacyWebSocket<String> socket() {
+        return WebSocket.whenReady((in, out) -> {
+            // For each event received on the socket,
+            in.onMessage(System.out::println);
+
+            // When the socket is closed.
+            in.onClose(() -> System.out.println("Disconnected"));
+
+
+            // Send a single 'Hello!' message
+            out.write("Hello!");
+
+
+        });
     }
 
 }
